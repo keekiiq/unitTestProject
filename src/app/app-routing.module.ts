04156dd7a3471e1f9174cfe9db72d654ad837e02
TASANAI PINGMUEANG <tasanaikq@gmail.com>
2018-05-01 04:40:42 +0700

    Change app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { RegistrationComponent } from './registration/registration.component';
+import { CourseComponent } from './course/course.component';
 
 const routes: Routes = [
-  {
-  path: '',
-  component: HomeComponent
-  },
+
   {
   path: 'registration',
-  component: RegistrationComponent
+  component: CourseComponent
   }
   ];
 
